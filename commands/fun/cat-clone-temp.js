9acb33e51db8b35b528f089c71d61bfe4190f739
pinktabbyhunterlargo <codyrsumner@gmail.com>
2022-10-10 19:25:13 +0200

    a

--- commands/fun/cat-clone-temp.js
+++ commands/fun/cat-clone-temp.js
@@ -3,7 +3,9 @@ import Command from "../../classes/command.js";
 
 class FilePostCommand extends Command {
   async run() {
-    return "test";
+    const baseUrl = "http://bbfj.ddns.net/img-api/";
+
+    return "(success) - baseUrl=" + baseUrl;
   }
 
   static description = "there's no time to explain shark park lore";
